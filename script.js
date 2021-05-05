6a3d21e990f077865bd817d2836d9eb5caa65932
Justin Berger Howes <howes.j.j@gmail.com>
2021-05-05 18:30:22 +1000

    Adjusted a few elements

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 const pageBody = document.body.style;
 pageBody.backgroundColor = 'lightskyblue';
 pageBody.textAlign = "center";
+
+const github = document.getElementById('github');
+github.style.textDecoration = 'none';
+
+const contact = document.getElementById('contact');
+contact.style.textDecoration = 'none';
