4adc95101769387e02bc4d3192b23f3378457937
Justin Berger Howes <howes.j.j@gmail.com>
2021-05-06 00:27:34 +1000

    added toggle section - working on footer

--- script.js
+++ script.js
@@ -4,6 +4,33 @@ pageBody.textAlign = "center";
 
 const github = document.getElementById('github');
 github.style.textDecoration = 'none';
-
 const contact = document.getElementById('contact');
 contact.style.textDecoration = 'none';
+const me = document.getElementById('me');
+me.addEventListener('click', function(){
+    me.style.display = 'block'
+})
+
+const contactMe = document.getElementById('contactMe');
+const innerContact = document.getElementById('innerContact');
+const hello = document.getElementById('hello');
+
+contactMe.style.cursor = 'pointer';
+hello.style.cursor = 'pointer';
+
+
+contactMe.addEventListener('click', function(){
+    if (innerContact.style.display === 'none'){
+    innerContact.style.display = 'block';
+    } else {
+        innerContact.style.display = 'none';
+    }
+})
+
+hello.addEventListener('click', function(){
+    if (me.style.display === 'none'){
+    me.style.display = 'block';
+    } else {
+        me.style.display = 'none';
+    }
+})
