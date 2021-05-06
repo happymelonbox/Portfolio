b977372b2e9f8698b6a3b993473bf31b3e879563
Justin Berger Howes <howes.j.j@gmail.com>
2021-05-06 16:19:18 +1000

    finalised event listeners

--- script.js
+++ script.js
@@ -1,36 +1,41 @@
 const pageBody = document.body.style;
-pageBody.backgroundColor = 'lightskyblue';
-pageBody.textAlign = "center";
-
 const github = document.getElementById('github');
-github.style.textDecoration = 'none';
 const contact = document.getElementById('contact');
-contact.style.textDecoration = 'none';
 const me = document.getElementById('me');
-me.addEventListener('click', function(){
-    me.style.display = 'block'
-})
-
 const contactMe = document.getElementById('contactMe');
 const innerContact = document.getElementById('innerContact');
 const hello = document.getElementById('hello');
+const footer = document.getElementById('footer');
+const ack = document.getElementById('acknowledge')
 
+contact.style.textDecoration = 'none';
+github.style.textDecoration = 'none';
+pageBody.backgroundColor = 'lightskyblue';
+pageBody.textAlign = "center";
 contactMe.style.cursor = 'pointer';
 hello.style.cursor = 'pointer';
 
 
-contactMe.addEventListener('click', function(){
+helloTog();
+function helloTog(){
+    if (me.style.display === 'none'){
+    me.style.display = 'block';
+    footer.style.display = 'none'
+    } else {
+        me.style.display = 'none';
+        footer.style.display = 'block';
+    }
+}
+hello.addEventListener('click', helloTog)
+
+contactTog();
+function contactTog(){
     if (innerContact.style.display === 'none'){
     innerContact.style.display = 'block';
     } else {
         innerContact.style.display = 'none';
     }
-})
+}
+contactMe.addEventListener('click', contactTog)
 
-hello.addEventListener('click', function(){
-    if (me.style.display === 'none'){
-    me.style.display = 'block';
-    } else {
-        me.style.display = 'none';
-    }
-})
+
